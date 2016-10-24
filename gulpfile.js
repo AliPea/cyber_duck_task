07fb92b270fcd39a181505bf74a50052814c2ed9
AliPea <pearseindeas@gmail.com>
2016-10-24 21:11:39 +0100

    Working on the task for cyber_duck

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp     	 = require("gulp");
+const sass 		 	 = require('gulp-sass');
+const cleanCSS 	 = require("gulp-clean-css");
+const flatten		 = require('gulp-flatten');
+const plumber    = require('gulp-plumber');
+const nodemon    = require("gulp-nodemon");
+const livereload = require('gulp-livereload');
+
+const src  = "src";
+const dist = "public";
+
+gulp.task('nodemon', () => {
+  return nodemon({
+    script: 'server.js',
+    ext: 'js html',
+    env: { 'NODE_ENV': 'development' }
+  });
+});
+
+gulp.task('sass', () => {
+	return gulp.src(`${src}/**/*.scss`)
+    .pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS({ compatibility: "ie8"}))
+    .pipe(plumber())
+    .pipe(flatten())
+    .pipe(gulp.dest(`${dist}/css/`))
+    .pipe(livereload());
+});
+
+gulp.task("watch", () => {
+	livereload.listen();
+	gulp.watch(`${src}/**/*.scss`, ['sass']);
+});
+
+gulp.task("default", [
+  'sass',
+  'watch',
+  'nodemon'
+]);
---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+console.log("Loaded")
+
+// $("#searchbar").html('Subscribe');
+
+
+$(function(){
+  $('header nav > ul > li:nth-child(4) ul li:nth-child(1)').on('click', function(){
+    event.preventDefault();
+    $(this).siblings().slideToggle();
+  });
+});
