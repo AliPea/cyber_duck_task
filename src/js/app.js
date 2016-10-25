592b14003ad9bb1f4ed50795cfdf8373f62a6efe
AliPea <pearseindeas@gmail.com>
2016-10-25 08:45:21 +0100

    last commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,15 @@ console.log("Loaded")
 
 // $("#searchbar").html('Subscribe');
 
-
-$(function(){
-  $('header nav > ul > li:nth-child(4) ul li:nth-child(1)').on('click', function(){
-    event.preventDefault();
-    $(this).siblings().slideToggle();
-  });
-});
+// $(function(){
+//   $('header img > ul > li:nth-child(4) ul li:nth-child(1)').on('click', function(){
+//     event.preventDefault();
+//     $(this).siblings().slideToggle();
+//   });
+// });
+//
+// $(document).on('click','.navbar-collapse.in',function(e) {
+//   if( $(e.target).is('a') ) {
+//       $(this).collapse('hide');
+//   }
+// });
